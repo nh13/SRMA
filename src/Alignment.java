dc6e9e2cf81d43330363ed8bd4549d9e1756fe5b
Nils Homer <nilshomer@gmail.com>
2010-03-25 15:03:22 -0700

    * 5' + strand justifies incoming alignments.

--- src/Alignment.java
+++ src/Alignment.java
@@ -82,5 +82,100 @@ public class Alignment {
                 index++;
             }
         }
+
+        // left justify
+        this.leftJustify();
+
+    }
+
+    private void leftJustify()
+    {
+        // Left-justify alignment
+        int i;
+        int prevDel, prevIns, startDel, endDel, startIns, endIns;
+        
+        i = prevDel = prevIns = 0;
+        startDel = endDel = startIns = endIns = -1;
+
+        while(i<this.length) {
+            assert (0 == prevIns || 0 == prevDel);
+
+            if(Alignment.GAP == this.read[i]) {
+                if(0 == prevDel) {
+                    startDel = i;
+                }
+                prevDel = 1;
+                endDel = i;
+                prevIns = 0;
+                startIns = -1;
+                endIns = -1;
+                i++;
+            }
+            else if(Alignment.GAP == this.reference[i]) {
+                if(0 == prevIns) {
+                    startIns = i;
+                }
+                prevIns = 1;
+                endIns = i;
+                prevDel = 0;
+                startDel = -1;
+                endDel = -1;
+                i++;
+            }
+            else {
+                if(1 == prevDel) {
+                    assert (0 < startDel);
+                    assert (startDel <= endDel);
+                    startDel--;
+                    while(0 <= startDel && /* Bases remaining to examine */
+                            this.read[startDel] != Alignment.GAP && /* Hit another deletion */
+                            this.reference[startDel] != Alignment.GAP && /* Hit an insertion */
+                            this.reference[startDel] == this.reference[endDel]) { /* src ref base matches dest ref base */
+                        assert (Alignment.GAP != this.reference[startDel]);
+                        assert (Alignment.GAP != this.reference[endDel]);
+                        assert (Alignment.GAP != this.read[startDel]);
+                        assert (Alignment.GAP == this.read[endDel]);
+                        this.read[endDel] = this.read[startDel];
+                        this.read[startDel] = Alignment.GAP;
+                        startDel--;
+                        endDel--;
+                            }
+                    endDel++; /* We decremented when we exited the loop */
+                    i = endDel;
+                    assert (Alignment.GAP != this.read[i]);
+                    assert (Alignment.GAP != this.reference[i]);
+                }
+                else if(1 == prevIns) {
+                    assert (startIns <= endIns);
+                    startIns--;
+                    while(0 <= startIns && /* Bases remaining to examine */
+                            this.read[startIns] != Alignment.GAP && /* Hit another deletion */
+                            this.reference[startIns] != Alignment.GAP && /* Hit an insertion */
+                            this.read[startIns] == this.read[endIns]) { /* src this.read base matches dest this.read base */
+                        assert (Alignment.GAP != this.read[startIns]);
+                        assert (Alignment.GAP != this.read[endIns]);
+                        assert (Alignment.GAP != this.reference[startIns]);
+                        assert (Alignment.GAP == this.reference[endIns]);
+                        this.reference[endIns] = this.reference[startIns];
+                        this.reference[startIns] = Alignment.GAP;
+                        startIns--;
+                        endIns--;
+                            }
+                    endIns++; /* We decremented when we exited the loop */
+                    i = endIns;
+                    assert (Alignment.GAP != this.read[i]);
+                    assert (Alignment.GAP != this.reference[i]);
+                }
+                else {
+                    i++;
+                }
+                prevDel = 0;
+                prevIns = 0;
+                startDel = -1;
+                endDel = -1;
+                startIns = -1;
+                endIns = -1;
+            }
+        }
     }
 }
