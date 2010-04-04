26689d29ff57c3622e1ccd2d0f25db34c2f3de7a
Nils Homer <nilshomer@gmail.com>
2010-04-03 22:45:07 -0700

    * adding in newline.

--- scripts/srma.submit.pl
+++ scripts/srma.submit.pl
@@ -256,7 +256,7 @@ sub CreateJobs {
 			$outID= $1;
 		}
 		die unless (0 < length($outID));
-		print STDERR "[srma submit] hold removed QSUBID=$outID";
+		print STDERR "[srma submit] hold removed QSUBID=$outID\n";
 	}
 }
 
