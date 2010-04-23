a54c540b12f71cebdd931b296941e8c5e6ad3236
Nils Homer <nilshomer@gmail.com>
2010-04-22 23:55:48 -0700

    * moving master to 0.1.4.

--- src/SRMA.java
+++ src/SRMA.java
@@ -21,7 +21,7 @@ import java.lang.Math;
 
 public class SRMA extends CommandLineProgram { 
 
-    public final String PROGRAM_VERSION="0.1.3";
+    public final String PROGRAM_VERSION="0.1.4";
     @Usage (programVersion=PROGRAM_VERSION)
         public final String USAGE = getStandardUsagePreamble() + "Short read micro re-aligner.";
     @Option(shortName=StandardOptionDefinitions.INPUT_SHORT_NAME, doc="The input SAM or BAM file.")
